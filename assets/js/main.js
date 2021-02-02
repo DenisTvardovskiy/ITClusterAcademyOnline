994c989c021cca210322adf3e18f35f9348bc9d3
DenisTvardovskiy <40178797+DenisTvardovskiy@users.noreply.github.com>
2021-02-02 21:20:29 +0200

    recent photos done

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,16 @@ $(window).scroll(function() {
         $(".navigation").removeClass("scroll");
     }
 });
+
+
+
+var counter = 0;
+var op = ["0", '-50%', "-100%", "-150%", "-200%", "-150%", "-100%", "-50%"];
+setInterval(_=>{
+    document.documentElement.style.setProperty("--slider", op[counter]);
+    counter++;
+    if(counter > 8){
+        counter =0;
+        
+    }
+}, 5000);
